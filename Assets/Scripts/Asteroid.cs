cd136d3fec06ce586f88caf4731ecbcaa9fbefca
Dmytro <112495732+Soffaman@users.noreply.github.com>
2023-09-25 10:40:14 +0300

    Asteroids formed

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,17 @@
+
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
+        if (playerHealth == null)
+        {
+            return;
+        }
+
+        playerHealth.Crash();
+    }
+}
