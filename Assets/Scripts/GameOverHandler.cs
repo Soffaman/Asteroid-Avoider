17224c7587f495071840954d5139eb1d9e83a0cf
Dmytro <112495732+Soffaman@users.noreply.github.com>
2023-09-26 17:07:20 +0300

    Revert "Score System formed"
    This reverts commit 6929fc766f2f6248b6b5c45c0c8aa068db15bd4d.

--- Assets/Scripts/GameOverHandler.cs
+++ Assets/Scripts/GameOverHandler.cs
@@ -1,20 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOverHandler : MonoBehaviour
 {
-    [SerializeField] private TMP_Text _gameOverText;
-    [SerializeField] private ScoreSystem _scoreSystem;
     [SerializeField] private GameObject _gameOverDisplay;
     [SerializeField] private AsteroidSpawner _asteroidSpawner; 
     public void EndGame()
     {
-        int finalScore = _scoreSystem.EndTimer();
-        _gameOverText.text = $"Your Score: {finalScore}";
-
         _asteroidSpawner.enabled = false;
         _gameOverDisplay.gameObject.SetActive(true);
     }
